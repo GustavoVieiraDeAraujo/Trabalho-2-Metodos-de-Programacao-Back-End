813ee64e749f041b53275edc663043b59f5d5ea9
Luiz Henrique <luizhfs09@gmail.com>
2023-02-16 12:33:00 -0300

    fix: removendo senha do serializer e login via POST

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -8,13 +8,11 @@ class UserSerializer < ActiveModel::Serializer
   #   @return [String] o nome do usuário
   # @!attribute [rw] enrollment
   #   @return [Integer] o número de matrícula do usuário
-  # @!attribute [rw] password
-  #   @return [String] a senha do usuário
   # @!attribute [rw] is_admin
   #   @return [Boolean] se o usuário é um administrador
   # @!attribute [rw] is_student
   #   @return [Boolean] se o usuário é um estudante
   # @!attribute [rw] is_teacher
   #   @return [Boolean] se o usuário é um professor
-  attributes :id, :name, :enrollment, :password, :is_admin, :is_student, :is_teacher
+  attributes :id, :name, :enrollment, :is_admin, :is_student, :is_teacher
 end
